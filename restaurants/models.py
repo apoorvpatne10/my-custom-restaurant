b5d2d3ebf02a50ce73841a4a5a3885d26b579467
Apoorv Patne <31184004+apoorvpatne10@users.noreply.github.com>
2018-07-15 16:53:04 +0530

    Update models.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,8 +3,8 @@ from django.db import models
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
 from .validators import validate_category
-from django.core.urlresolvers import reverse
 from django.db.models import Q
+from django.core.urlresolvers import reverse
 
 User = settings.AUTH_USER_MODEL
 
