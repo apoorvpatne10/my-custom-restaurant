27448d49ca034d976e31e8e82e4e80089a04201f
Apoorv Patne <Apoorv.Patne10@gmail.com>
2018-07-15 16:50:08 +0530

    Modifed feed-page, improved search feature, added account activation

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -2,14 +2,18 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from restaurants import views
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView
-from profiles.views import ProfileFollowToggle
+from django.contrib.auth.views import LoginView, LogoutView
+from profiles.views import ProfileFollowToggle, RegisterView, activate_user_view
+from menus.views import HomeView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'^activate/(?P<code>[a-z0-9].*)/$', activate_user_view, name='activate'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
     url(r'^u/', include('profiles.urls', namespace='profile')),
     url(r'^items/', include('menus.urls', namespace='menus')),
